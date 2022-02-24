9e8690061b1d2b7cd1c6ca9c69c99c7e81ecba08
briannelson95 <71141646+briannelson95@users.noreply.github.com>
2022-02-23 20:32:51 -0500

    organizing

--- schemas/trail.js
+++ schemas/trail.js
@@ -13,10 +13,44 @@ export default {
             type: 'string',
         },
         {
+            name: 'difficulty',
+            title: 'Difficulty',
+            type: 'string',
+            options: {
+                list: [
+                    {title: 'Easy', value: 'easy'},
+                    {title: 'Moderate', value: 'moderate'},
+                    {title: 'Hard', value: 'hard'}
+                ]
+            } 
+        },
+        {
+            name: 'distance',
+            title: 'Distance',
+            type: 'number'
+        },
+        {
             name: 'rating',
             title: 'Rating',
             type: 'rating',
             description: 'Rate this Trail out of 5 Stars'
         },
+        {
+            name: 'trailType',
+            title: 'Trail Type',
+            type: 'string',
+            options: {
+                list: [
+                    {title: 'Out & Back', value: 'outBack'},
+                    {title: 'Loop', value: 'loop'},
+                    {title: 'Point to Point', value: 'point'},
+                ]
+            }
+        },
+        {
+            name: 'loaction',
+            title: 'Location',
+            type: 'geopoint'
+        }
     ],
 }
